3939ffe0af7cfcd4f702d71e77749164cd2e64b7
Janishar Akhtar <janisharakhtar277@gmail.com>
2023-10-27 17:10:54 +0530

    My first commit

---
+++ JavaBeg/src/Concatenation/Class.java
@@ -0,0 +1,22 @@
+package Concatenation;
+
+public class Class {
+    public static void main(String[] args){
+        String str = "Janishar";
+        String str1 = "Akhtar";
+        // we are just adding two string
+        System.out.println(str+" "+str1);
+        // concatenate thi two strong
+        System.out.println(str.concat(str1));
+
+        String x = "10";
+        String y = "20";
+        String z = x + y;  // z will be 1020 (a String)
+        System.out.println(z);
+
+        int x1 = 10;
+        int y1 = 20;
+        String z1 = x + y;  // z will be 1020 (a String)
+        System.out.println(z1);
+    }
+}
---
+++ JavaBeg/src/Math/Class.java
@@ -0,0 +1,13 @@
+package Math;
+
+public class Class {
+    public static void main(String[] args) {
+        //int
+        //Math.max(5, 10);
+        System.out.println(Math.max(5, 10));
+        System.out.println(Math.min(5, 10));
+        System.out.println(Math.sqrt(5));
+        System.out.println(Math.abs(10));
+
+    }
+}
