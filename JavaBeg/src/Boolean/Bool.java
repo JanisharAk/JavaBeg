4ec0212d7a8952eb9c952d093b388b4147be2c15
Janishar Akhtar <janisharakhtar277@gmail.com>
2023-10-27 19:24:20 +0530

    My first commit

---
+++ JavaBeg/src/Boolean/Bool.java
@@ -0,0 +1,13 @@
+package Boolean;
+
+public class Bool {
+    public static void main(String[] args) {
+
+        //A boolean type is declared with the boolean keyword and can only take the values true or false
+
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println(isJavaFun);     // Outputs true
+        System.out.println(isFishTasty);   // Outputs false
+    }
+}
---
+++ JavaBeg/src/IfStatement/Class.java
@@ -0,0 +1,22 @@
+package IfStatement;
+
+public class Class {
+    public static void main(String[] args) {
+       // The Java if statement tests the condition. It executes the if block if condition is true.
+
+        int numOfTech = 20;
+        if(numOfTech > 10){
+            System.out.println("numOftech is greater");
+        }
+
+        int num = 100;
+        if(num < 100){
+            System.out.println("Lesser");
+        }
+        // there is no need to assign anything in else part
+
+        else{
+            System.out.println("Greater");
+        }
+    }
+}
